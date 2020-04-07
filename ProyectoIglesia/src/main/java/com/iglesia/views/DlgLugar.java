2153b14df52b3211884b3af023a150a3d7a50b13
Alexis <alexis.mezquita@outlook.com>
2020-04-07 12:51:59 -0600

    Commit temas de pantallas

--- ProyectoIglesia/src/main/java/com/iglesia/views/DlgLugar.java
+++ ProyectoIglesia/src/main/java/com/iglesia/views/DlgLugar.java
@@ -39,7 +39,6 @@ public class DlgLugar extends javax.swing.JDialog {
         this.txtnombre.requestFocus();
         new TextPrompt("Digite nombre para buscar", this.txtbuscar);
         this.cbestado.setSelected(true);
-        this.jPanel1.setBorder(BorderFactory.createLineBorder(Color.LIGHT_GRAY));
     }
     private void mostrarTabla(String nombre) {
         this.lugarController.llenarTabla(this.jtLugar, nombre);
@@ -97,12 +96,14 @@ public class DlgLugar extends javax.swing.JDialog {
         txtbuscar = new javax.swing.JTextField();
         jSeparator1 = new javax.swing.JSeparator();
         jSeparator2 = new javax.swing.JSeparator();
+        jLabel4 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setUndecorated(true);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 129, 255)));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -131,7 +132,7 @@ public class DlgLugar extends javax.swing.JDialog {
                 txtnombreActionPerformed(evt);
             }
         });
-        jPanel1.add(txtnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 120, 360, 24));
+        jPanel1.add(txtnombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 120, 320, 24));
 
         cbestado.setBackground(new java.awt.Color(255, 255, 255));
         cbestado.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
@@ -142,7 +143,7 @@ public class DlgLugar extends javax.swing.JDialog {
                 cbestadoActionPerformed(evt);
             }
         });
-        jPanel1.add(cbestado, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 170, 90, -1));
+        jPanel1.add(cbestado, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 170, 90, -1));
 
         jbingresar.setBackground(new java.awt.Color(0, 129, 255));
         jbingresar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -158,7 +159,7 @@ public class DlgLugar extends javax.swing.JDialog {
                 jbingresarActionPerformed(evt);
             }
         });
-        jPanel1.add(jbingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 200, 130, 40));
+        jPanel1.add(jbingresar, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 200, 130, 40));
 
         jblimpiar.setBackground(new java.awt.Color(254, 192, 1));
         jblimpiar.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
@@ -174,11 +175,9 @@ public class DlgLugar extends javax.swing.JDialog {
                 jblimpiarActionPerformed(evt);
             }
         });
-        jPanel1.add(jblimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 200, 130, 40));
+        jPanel1.add(jblimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(175, 200, 130, 40));
 
-        jtLugar.setBackground(new java.awt.Color(224, 224, 224));
         jtLugar.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
-        jtLugar.setForeground(new java.awt.Color(0, 0, 0));
         jtLugar.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
@@ -202,8 +201,7 @@ public class DlgLugar extends javax.swing.JDialog {
                 return canEdit [columnIndex];
             }
         });
-        jtLugar.setSelectionBackground(new java.awt.Color(189, 189, 189));
-        jtLugar.setSelectionForeground(new java.awt.Color(51, 51, 51));
+        jtLugar.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jtLugar.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jtLugarMouseClicked(evt);
@@ -211,9 +209,9 @@ public class DlgLugar extends javax.swing.JDialog {
         });
         jScrollPane1.setViewportView(jtLugar);
         if (jtLugar.getColumnModel().getColumnCount() > 0) {
-            jtLugar.getColumnModel().getColumn(0).setMinWidth(50);
-            jtLugar.getColumnModel().getColumn(0).setPreferredWidth(50);
-            jtLugar.getColumnModel().getColumn(0).setMaxWidth(50);
+            jtLugar.getColumnModel().getColumn(0).setMinWidth(30);
+            jtLugar.getColumnModel().getColumn(0).setPreferredWidth(30);
+            jtLugar.getColumnModel().getColumn(0).setMaxWidth(45);
             jtLugar.getColumnModel().getColumn(2).setMinWidth(75);
             jtLugar.getColumnModel().getColumn(2).setPreferredWidth(75);
             jtLugar.getColumnModel().getColumn(2).setMaxWidth(75);
@@ -233,7 +231,7 @@ public class DlgLugar extends javax.swing.JDialog {
                 jbsalirActionPerformed(evt);
             }
         });
-        jPanel1.add(jbsalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(350, 0, 30, 30));
+        jPanel1.add(jbsalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(349, 1, 30, 30));
 
         txtid.setEditable(false);
         txtid.setBackground(new java.awt.Color(255, 255, 255));
@@ -252,9 +250,12 @@ public class DlgLugar extends javax.swing.JDialog {
                 txtbuscarKeyReleased(evt);
             }
         });
-        jPanel1.add(txtbuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 250, 350, 24));
-        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 145, 360, -1));
-        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 275, 350, -1));
+        jPanel1.add(txtbuscar, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 250, 320, 24));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 144, 320, -1));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 275, 320, -1));
+
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/META-INF/images/icon/buscar-b.png"))); // NOI18N
+        jPanel1.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 255, -1, -1));
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 380, 470));
 
@@ -347,6 +348,7 @@ public class DlgLugar extends javax.swing.JDialog {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
